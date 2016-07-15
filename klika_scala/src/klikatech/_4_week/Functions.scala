29063e26819b70b07b10eab3c08b498f56e2455d
Dzmitry Chaulytka <dchavlytko@gmail.com>
2016-07-15 17:12:47 +0300

    week 4 hw

---
+++ klika_scala/src/klikatech/_4_week/Functions.scala
@@ -0,0 +1,59 @@
+package klikatech._4_week
+
+object Function {
+
+  /**
+   * 1. Higher order functions: Напишите прокси-генератор: функцию, которая принимает на вход две функции: f и g и возвращает
+   * функцию, которая применяет g к результату выполнения f и возвращает результат выполнения f.
+   */
+  def proxyGenerator[T, R](f: T => R, g: R => Unit) = (t: T) => {
+    val res = f(t)
+    g(res)
+    res
+  }
+
+  /**
+   * Alternative implementation
+   */
+  def proxyGenerator2[T, R](g: R => Unit)(f: T => R) = (t: T) => {
+    val res = f(t)
+    g(res)
+    res
+  }
+
+  /**
+   * 2. Function literal: Напишите функцию, котороя принимает на вход список натуральных чисел и возвращает список,
+   * содержащий элементы первого списка возведённые в квадрат. Использование циклов не допускается.
+   */
+  def squaredSeq(seq: Seq[Int]) = seq.map(x => x * x)
+
+  /**
+   * 3. Partially applied functions: Используя прокси-генератор из первого задания напишите функцию-обёртку, выводящую в
+   * консоль результаты выполнения любой функции.
+   */
+  def loggingWrapper[T, R] = proxyGenerator(_: T => R, println)
+
+  /**
+   * Alternative implementation
+   */
+  def loggingWrapper2[T, R] = proxyGenerator2[T, R](println) _
+
+  /**
+   * 4. Partial functions: Напишите функцию, вычисляющую квадратный корень любого неотрицательного числа.
+   */
+  val sqrt: PartialFunction[Double, Double] = {
+    case d: Double if d > -1 => math.sqrt(d)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val res = loggingWrapper[Int, Int](_ * 2)(4)
+    println(res)
+    val res2 = loggingWrapper2[Int, Boolean](_ > 7)(4)
+    println(res2)
+
+    println(squaredSeq(Seq(1, 2, 3, 4, 5, 6)))
+
+    println(sqrt(4))
+    println(sqrt(-1))
+  }
+}
